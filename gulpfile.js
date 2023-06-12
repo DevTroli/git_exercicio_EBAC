ed66c4479c485f326efee3de471daee0fe9ab5d5
Troli <119256727+DevTroli@users.noreply.github.com>
2023-06-12 12:05:36 -0300

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,29 +1,28 @@
 const gulp = require ('gulp');
-// const uglify = require ('gulp-uglify');
-// const sass = require ('gulp-sass')(require('sass'));
-import imagemin from 'gulp-imagemin';
-
-
-
-function images() {
+const uglify = require ('gulp-uglify');
+const sass = require ('gulp-sass')(require('sass'));
+const imagemin = require ('gulp-imagemin');
+async function images() {
+  const imagemin = (await import('gulp-imagemin')).default;
   return gulp.src('./src/images/*')
-    .pipe(imageMin())
+    .pipe(imagemin())
     .pipe(gulp.dest('./build/images'));
-}
-// function soma () {
-//   return gulp.src('./src/scripts/soma.js')
-//   .pipe(uglify())
-//   .pipe(gulp.dest('./build/scripts'));
-// }
+  };
+  
+ function soma () {
+   return gulp.src('./src/scripts/soma.js')
+   .pipe(uglify())
+   .pipe(gulp.dest('./build/scripts'));
+ }
 
-//  gulp.task('sass', function Sass() {
-//      return gulp.src('./src/styles/main.scss')
-//        .pipe(sass())
-//        .pipe(gulp.dest('./build/styles'))
-//        ;
-//    });
+  function sass() {
+      return gulp.src('./src/styles/main.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./build/styles'))
+        ;
+    };
   
 
-// exports.soma= soma;
-// exports.Sass = sass;
+exports.soma= soma;
+exports.sass = sass;
 exports.images = images;
