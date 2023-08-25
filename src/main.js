15cb56272e0e49d35dcb8dcf2bd19875fc2b7a88
Troli <brawlixo123@gmail.com>
2023-08-25 18:57:54 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,35 @@
+"use strict";
+let peso = 90;
+let altura = 1.90;
+let nome = "Pablo";
+// arrow function em TS usando a formula do IMC 
+const IMC = (peso, altura) => {
+    return peso / (altura * altura);
+};
+function StatusPeso(peso) {
+    if (peso <= 18.5) {
+        return "Abaixo do peso";
+    }
+    else if (peso > 18.5 && peso <= 25) {
+        return "Peso ideal";
+    }
+    else if (peso > 25 && peso <= 30) {
+        return "Sobrepeso";
+    }
+    else if (peso > 30 && peso <= 35) {
+        return "Obesidade grau 1";
+    }
+    else if (peso > 35 && peso <= 40) {
+        return "Obesidade grau 2";
+    }
+    else {
+        return "Obesidade grau 3";
+    }
+}
+// verificação da função IMC esta tudo e uso de toFixed para usoo de só duas casas decimais do total
+console.log(nome + " sua altura é: " + altura + " de altura e sua peso é: " + peso + " kgs, logo seu IMC é " + IMC(peso, altura).toFixed(1));
+console.log(StatusPeso(IMC(peso, altura)));
+function Agredecimentos(nome) {
+    return "olá" + nome + ", Obrigado por usar o nosso sistema para o calculo do seu IMC! Que você tenha um bom dia!";
+}
+console.log(Agredecimentos("Pablo"));
